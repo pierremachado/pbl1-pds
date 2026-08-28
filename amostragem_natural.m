2c3e202783179255e9f4392201f6ae71beaea6b0
Pierre Machado <pierremachado@proton.me>
2026-08-28 12:55:43 -0300

    fix: padronização da forma de geração de vetor

--- amostragem_natural.m
+++ amostragem_natural.m
@@ -4,7 +4,7 @@ function [xs, ts] = amostragem_natural(x, Fs, w, Ti = 0, Tf)
   % Ti = tempo inicial (default = 0), Tf = Tempo final
   % saída: xs = vetor de amostras
   Ts = 1/Fs; % período de amostragem
-  ts = Ti:0.0001:Tf;
+  ts = Ti:Ts/1000:Tf;
   y = pulstran (ts, Ti:Ts:Tf, "rectpuls", w)
 
   xs = x(ts).*y;
